61089fdac6f0f6f25723ceb04e630d199acaa80b
shweta-585 <sweta.app19@gmail.com>
2024-11-26 11:04:35 +0530

    Created uuid generation file

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 # install dependable libraries
 # -y say yes to all prompt messages
 # /dev/null doesn't show installation process
-sudo apt-get install -y libssl-dev > /dev/null 2>&1
+sudo apt-get install -y libssl-dev uuid-dev > /dev/null 2>&1
 
 cc -Wall main.c logic.c peer.c -o run -lssl -lcrypto
 
